1cb9b54b5b057beab8277256c29a84ed092fa8c1
chrischep <christine.chepleting@student.moringaschool.com>
2022-08-30 09:01:32 +0300

    lab incomplete

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ function isPalindrome(word) {
 /*
   Add written explanation of your solution here
 */
+function reverseString(term) {
+  const termArray = term.split("");
+  const reverseTermArray = termArray.reverse();
+  const reverseTerm = reverseTermArray.join("");
+ return term;
+}
+function isPalindrome(term){
+  const reverseTerm=reverseString(term)
+  if(term==reverseTerm){
+    return true}
+    else{return false}
+}
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
